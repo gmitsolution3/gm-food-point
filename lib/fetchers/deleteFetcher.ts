73195c24d6ef4bd459c6d574b0742a953702bcf7
gmitsolution3 <gmitsolution3@gmail.com>
2026-07-01 17:59:30 +0600

    hello

--- lib/fetchers/deleteFetcher.ts
+++ lib/fetchers/deleteFetcher.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "../axios";
 export const deleteFetcher = async (
   url: string,
   { arg }: { arg: string | number },
-) => {
+) => { 
   const res = await axiosInstance.delete(`${url}/${arg}`);
   return res.data;
 };
